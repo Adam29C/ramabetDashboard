f15c41ffe6abf72fec747f5427fac4a6f8e101fc
Adam29C <adamcatholic29@gmail.com>
2024-07-01 11:20:16 +0530

    fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './Components/Redux/store/Store.jsx'
-// import store from "./Components/redux/store"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+
 <Provider store={store}>
 <App />
 </Provider>
-  // </React.StrictMode>,
+
 )
