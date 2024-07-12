235e17b8cebb3115ebae5fc14f799daa0b27748f
Amreen <amreenkhangithup@gmail.com>
2024-07-12 12:19:00 +0530

    update game provider

--- src/Components/Helpers/Provider/GameProviderAdd.jsx
+++ src/Components/Helpers/Provider/GameProviderAdd.jsx
@@ -1,147 +1,142 @@
-import React from 'react'
-import PagesIndex from '../../Pages/PagesIndex'
+import React from "react";
+import PagesIndex from "../../Pages/PagesIndex";
 
 const GameProviderAdd = () => {
-  const userId = localStorage.getItem("userId")
-const navigate = PagesIndex.useNavigate()
-const location = PagesIndex.useLocation()
-console.log(location?.state)
+  const userId = localStorage.getItem("userId");
+  const navigate = PagesIndex.useNavigate();
+  const location = PagesIndex.useLocation();
+  console.log(location?.state);
   const formik = PagesIndex.useFormik({
-    initialValues:{
+    initialValues: {
       game: location?.state ? location?.state?.game : "",
       providerName: location?.state ? location?.state?.providerName : "",
-      providerResult:location?.state ? location?.state?.providerResult : "",
-      resultStatus:0,
- 
-      mobile:location?.state ? location?.state?.mobile : "",
-      activeStatus:location?.activeStatus ? location?.state?.activeStatus :false,
-   
+      providerResult: location?.state ? location?.state?.providerResult : "",
+      resultStatus: 0,
 
+      mobile: location?.state ? location?.state?.mobile : "",
+      activeStatus: location?.activeStatus
+        ? location?.state?.activeStatus
+        : false,
     },
-    validate:(values)=>{
-const errors = {}
-      if(!values.game){
-        errors.game = PagesIndex.valid_err.GAME_NAME_ERROR
+    validate: (values) => {
+      const errors = {};
+      if (!values.game) {
+        errors.game = PagesIndex.valid_err.GAME_NAME_ERROR;
       }
 
-      if(!values.providerName){
-        errors.providerName = PagesIndex.valid_err.PROVIDER_NAME_ERROR
+      if (!values.providerName) {
+        errors.providerName = PagesIndex.valid_err.PROVIDER_NAME_ERROR;
       }
 
-      if(!values.providerResult){
-        errors.providerResult = PagesIndex.valid_err.PROVIDER_RESULT_ERROR
+      if (!values.providerResult) {
+        errors.providerResult = PagesIndex.valid_err.PROVIDER_RESULT_ERROR;
       }
 
-      if(!values.mobile){
-        errors.mobile = PagesIndex.valid_err.CONTACT_ERROR
+      if (!values.mobile) {
+        errors.mobile = PagesIndex.valid_err.CONTACT_ERROR;
       }
- 
-return errors
+
+      return errors;
     },
 
-    onSubmit:async(values)=>{
-      console.log(values)
+    onSubmit: async (values) => {
       try {
-          let data = {
-            adminId : userId,
-            game: values.game,
+        let data = {
+          adminId: userId,
+          game: values.game,
           providerName: values.providerName,
-          providerResult:values.providerResult,
-          resultStatus:values.resultStatus,
-          activeStatus:values.activeStatus,
-          mobile:values.mobile,
-          activeStatus:values.activeStatus
-          }
+          providerResult: values.providerResult,
+          resultStatus: values.resultStatus,
+          activeStatus: values.activeStatus,
+          mobile: values.mobile,
+          activeStatus: values.activeStatus,
+          ...(location?.state?._id ? { gameProviderId: location?.state?._id } : ""),
+        };
+        console.log(data);
 
-          
-    
-            const res = await PagesIndex.admin_services.GAME_PROVIDER_ADD_API(data)
+        const res =location?.state?._id ?await PagesIndex.admin_services.GAME_PROVIDER_UPDATE_API(data) : await PagesIndex.admin_services.GAME_PROVIDER_ADD_API(data);
 
-            if(res?.status === 200){
-              PagesIndex.toast.success(res?.message)
-              setTimeout(() => {
-                navigate("/admin/games")
-              }, 1000);
-            }else{
-              PagesIndex.toast.error(res.message)
-            }
-        } catch (error) {
-            PagesIndex.toast.error(error);
+        if (res?.status === 200) {
+          PagesIndex.toast.success(res?.message);
+          setTimeout(() => {
+            navigate("/admin/games");
+          }, 1000);
+        } else {
+          PagesIndex.toast.error(res.message);
         }
+      } catch (error) {
+        PagesIndex.toast.error(error);
+      }
+    },
+  });
 
-   
-    }
-  })
-
-
-const fields = [
+  const fields = [
     {
-        name:"game",
-        label:"Game",
-        type:"text",
-        label_size:6,
-        col_size:6
+      name: "game",
+      label: "Game",
+      type: "text",
+      label_size: 6,
+      col_size: 6,
     },
     {
-        name:"providerName",
-        label:"Provider Name",
-        type:"text",
-        label_size:6,
-        col_size:6
+      name: "providerName",
+      label: "Provider Name",
+      type: "text",
+      label_size: 6,
+      col_size: 6,
     },
     {
-      name:"providerResult",
-      label:"Provider Result",
-      type:"text",
-      label_size:6,
-      col_size:6
-  },
-//   {
-//     name:"resultStatus",
-//     label:"Result Status",
-//     type:"text",
-//     label_size:6,
-//     col_size:6
-// },
+      name: "providerResult",
+      label: "Provider Result",
+      type: "text",
+      label_size: 6,
+      col_size: 6,
+    },
+    //   {
+    //     name:"resultStatus",
+    //     label:"Result Status",
+    //     type:"text",
+    //     label_size:6,
+    //     col_size:6
+    // },
 
-
-{
-  name:"mobile",
-  label:"Mobile",
-  type:"number",
-  label_size:6,
-  col_size:6
-},
-
-
-{
-  name: "activeStatus",
-  label: "Active Status",
-  type: "checkbox",
-
-  title_size : 6,
-
-  options: [
     {
-      id: 1,
-      label: "activeStatus",
-      checked: formik.values.activeStatus,
-    }
-   
-  ],
-},
-]
+      name: "mobile",
+      label: "Mobile",
+      type: "number",
+      label_size: 6,
+      col_size: 6,
+    },
+
+    {
+      name: "activeStatus",
+      label: "Active Status",
+      type: "checkbox",
+
+      title_size: 6,
+
+      options: [
+        {
+          id: 1,
+          label: "activeStatus",
+          checked: formik.values.activeStatus,
+        },
+      ],
+    },
+  ];
   return (
     <PagesIndex.Main_Containt add_button={false} route="">
-<PagesIndex.Formikform fieldtype={fields.filter((field) => !field.showWhen)}
-          formik={formik}
+      <PagesIndex.Formikform
+        fieldtype={fields.filter((field) => !field.showWhen)}
+        formik={formik}
         //   btn_name={loding ? <PagesIndex.Loader text="Submit"/> : "Login"}
-          btn_name={"Add"}
-          button_Size={"w-10"}
-          show_submit={true}/>
-           <PagesIndex.Toast/>
+        btn_name={"Add"}
+        button_Size={"w-10"}
+        show_submit={true}
+      />
+      <PagesIndex.Toast />
     </PagesIndex.Main_Containt>
-  )
-}
+  );
+};
 
-export default GameProviderAdd
+export default GameProviderAdd;
